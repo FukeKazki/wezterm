c27148c4e76f46c0213419b8bb9f61bb84eef6e6
FukeKazki <kazkichi0906@gmail.com>
2023-07-23 21:30:51 +0900

    update-statusのアップデート

--- wezterm.lua
+++ wezterm.lua
@@ -3,7 +3,10 @@ local act = wezterm.action
 local config = {}
 
 -- Font
-config.font = wezterm.font("FiraCode Nerd Font")
+-- config.font = wezterm.font("FiraCode Nerd Font")
+config.font = wezterm.font("UDEV Gothic 35NFLG")
+-- リガチャの対応
+config.harfbuzz_features = { "calt=1", "clig=1", "liga=1" }
 config.font_size = 12.0
 -- Color scheme
 config.color_scheme = "Catppuccin Macchiato (Gogh)"
@@ -28,16 +31,16 @@ config.keys = {
 		action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
 	},
 	-- move pane
-	{ key = "LeftArrow",  mods = "SHIFT", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+	{ key = "LeftArrow", mods = "SHIFT", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
 	{ key = "RightArrow", mods = "SHIFT", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-	{ key = "UpArrow",    mods = "SHIFT", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-	{ key = "DownArrow",  mods = "SHIFT", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+	{ key = "UpArrow", mods = "SHIFT", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+	{ key = "DownArrow", mods = "SHIFT", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
 	-- move tab
-	{ key = "Tab",        mods = "CTRL",  action = wezterm.action({ ActivateTabRelative = -1 }) },
+	{ key = "Tab", mods = "CTRL", action = wezterm.action({ ActivateTabRelative = -1 }) },
 	-- copy
-	{ key = "c",          mods = "CMD",   action = wezterm.action.CopyTo("ClipboardAndPrimarySelection") },
+	{ key = "c", mods = "CMD", action = wezterm.action.CopyTo("ClipboardAndPrimarySelection") },
 	-- paste
-	{ key = "v",          mods = "CMD",   action = wezterm.action.PasteFrom("Clipboard") },
+	{ key = "v", mods = "CMD", action = wezterm.action.PasteFrom("Clipboard") },
 	-- Leaderキーを押した後にrを押すと、リサイズモードに入る
 	-- cmd + q
 	{
@@ -57,16 +60,16 @@ config.keys = {
 config.key_tables = {
 	-- リサイズモード
 	resize_pane = {
-		{ key = "LeftArrow",  action = act.AdjustPaneSize({ "Left", 1 }) },
-		{ key = "h",          action = act.AdjustPaneSize({ "Left", 2 }) },
+		{ key = "LeftArrow", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "h", action = act.AdjustPaneSize({ "Left", 2 }) },
 		{ key = "RightArrow", action = act.AdjustPaneSize({ "Right", 1 }) },
-		{ key = "l",          action = act.AdjustPaneSize({ "Right", 1 }) },
-		{ key = "UpArrow",    action = act.AdjustPaneSize({ "Up", 1 }) },
-		{ key = "k",          action = act.AdjustPaneSize({ "Up", 1 }) },
-		{ key = "DownArrow",  action = act.AdjustPaneSize({ "Down", 1 }) },
-		{ key = "j",          action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "UpArrow", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "DownArrow", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
 		-- Cancel the mode by pressing escape
-		{ key = "Escape",     action = "PopKeyTable" },
+		{ key = "Escape", action = "PopKeyTable" },
 	},
 }
 
@@ -82,14 +85,82 @@ config.window_decorations = "RESIZE"
 config.enable_tab_bar = true
 
 config.status_update_interval = 1000
-wezterm.on("update-status", function(window, pane)
-	-- wezterm.log_info(tab)
-	local cwd = pane:get_current_working_dir()
-	local right_status = ""
-	if cwd ~= nil then
-		right_status = cwd .. " " .. "hello "
-	end
-	window:set_right_status(right_status)
-end)
 
+wezterm.on("update-right-status", function(window, pane)
+	-- Each element holds the text for a cell in a "powerline" style << fade
+	local cells = {}
+
+	-- Figure out the cwd and host of the current pane.
+	-- This will pick up the hostname for the remote host if your
+	-- shell is using OSC 7 on the remote host.
+	local cwd_uri = pane:get_current_working_dir()
+	if cwd_uri then
+		cwd_uri = cwd_uri:sub(8)
+		local slash = cwd_uri:find("/")
+		local cwd = ""
+		local hostname = ""
+		if slash then
+			hostname = cwd_uri:sub(1, slash - 1)
+			-- Remove the domain name portion of the hostname
+			local dot = hostname:find("[.]")
+			if dot then
+				hostname = hostname:sub(1, dot - 1)
+			end
+			-- and extract the cwd from the uri
+			cwd = cwd_uri:sub(slash)
+
+			table.insert(cells, cwd)
+			table.insert(cells, hostname)
+		end
+	end
+
+	-- I like my date/time in this style: "Wed Mar 3 08:14"
+	local date = wezterm.strftime("%a %b %-d %H:%M")
+	table.insert(cells, date)
+
+	-- An entry for each battery (typically 0 or 1 battery)
+	for _, b in ipairs(wezterm.battery_info()) do
+		table.insert(cells, string.format("%.0f%%", b.state_of_charge * 100))
+	end
+
+	-- The filled in variant of the < symbol
+	local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+	-- Color palette for the backgrounds of each cell
+	local colors = {
+		"#3c1361",
+		"#52307c",
+		"#663a82",
+		"#7c5295",
+		"#b491c8",
+	}
+
+	-- Foreground color for the text across the fade
+	local text_fg = "#c0c0c0"
+
+	-- The elements to be formatted
+	local elements = {}
+	-- How many cells have been formatted
+	local num_cells = 0
+
+	-- Translate a cell into elements
+	local function push(text, is_last)
+		local cell_no = num_cells + 1
+		table.insert(elements, { Foreground = { Color = text_fg } })
+		table.insert(elements, { Background = { Color = colors[cell_no] } })
+		table.insert(elements, { Text = " " .. text .. " " })
+		if not is_last then
+			table.insert(elements, { Foreground = { Color = colors[cell_no + 1] } })
+			table.insert(elements, { Text = SOLID_LEFT_ARROW })
+		end
+		num_cells = num_cells + 1
+	end
+
+	while #cells > 0 do
+		local cell = table.remove(cells, 1)
+		push(cell, #cells == 0)
+	end
+
+	window:set_right_status(wezterm.format(elements))
+end)
 return config
